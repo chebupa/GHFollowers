2a14ef465a46bc7089142ed24d5067e0a88dd350
Aristarh Shakulin <aristarhsakulin@gmail.com>
2024-09-19 12:20:18 +0300

    initial commit

---
+++ GHFollowers/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  GHFollowers
+//
+//  Created by aristarh on 19.09.2024.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.title = "secondVC"
+    }
+}
--- GHFollowers/ViewController.swift
+++ GHFollowers/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "asd"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        view.backgroundColor = .white
+        setupButton()
     }
-
-
+    
+    func setupButton() {
+        view.addSubview(button)
+        
+        button.setTitle("asd", for: .normal)
+        button.setTitleColor(.systemPink, for: .normal)
+        button.backgroundColor = .black
+        button.layer.cornerRadius = 25
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 200),
+            button.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        button.addTarget(self, action: #selector(goToTheNextScreen), for: .touchUpInside)
+    }
+    
+    @objc func goToTheNextScreen() {
+        let nextScreen = SecondViewController()
+        navigationController?.pushViewController(nextScreen, animated: true)
+    }
+    
 }
 
