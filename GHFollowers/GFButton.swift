23fe6331991aec7065a501b517e9ac206af0a8db
chebupa <aristarhsakulin@gmail.com>
2024-11-08 14:05:22 +0300

    button update

--- GHFollowers/GFButton.swift
+++ GHFollowers/GFButton.swift
@@ -11,16 +11,28 @@ class GFButton: UIButton {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configure()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder) has not been implemented")
     }
     
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
     private func configure() {
         layer.cornerRadius = 10
         titleLabel?.textColor = .white
-        titleLabel?.font = .preferredFont(forTextStyle: .)
+//        titleLabel?.font = .preferredFont(forTextStyle: .)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            //
+        ])
     }
-
 }
